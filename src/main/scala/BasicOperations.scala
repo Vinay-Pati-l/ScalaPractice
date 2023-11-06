80cb5d07a7ac7c4bc67a04c1712d485598abbd24
Vinay Pati l <vspatilkondapur@gmail.com>
2023-11-06 17:42:43 +0530

    Made Changes

--- src/main/scala/BasicOperations.scala
+++ src/main/scala/BasicOperations.scala
@@ -1,12 +1,14 @@
 object BasicOperations {
 
-  def Add(x:Double, y:Double): Any = { x + y}
+  def Add(x: Double, y: Double): Double = {
+    x + y
+  }
   def AddString(x: String, y: String): String = {
     x + y
   }
-  def Subtract(x:Double, y:Double): Any = { x - y}
-  def Multiply(x:Double, y:Double): Any = { x * y}
-  def MultiplyString(x:String, y:Int): Any = { x * y}
+  def Subtract(x:Double, y:Double): Double = { x - y}
+  def Multiply(x:Double, y:Double): Double = { x * y}
+  def MultiplyString(x:String, y:Int): String = { x * y}
 
   def Divide(x: Double, y: Double): Any = {
     if (y == 0) {
